bbf3ade1e4d9b6628decda904e4d341f29e80e09
kmumladze <ketevanmumladze17@gmail.com>
2024-04-24 12:36:57 +0400

    style: hotel cards

--- app/page.js
+++ app/page.js
@@ -1,15 +1,17 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
   const [hotels, setHotels] = useState([]);
 
   useEffect(() => {
     async function fetchHotels() {
-      const response = await fetch('https://hotelbooking.stepprojects.ge/api/Hotels/GetAll');
+      const response = await fetch(
+        "https://hotelbooking.stepprojects.ge/api/Hotels/GetAll"
+      );
       const hotels = await response.json();
       setHotels(hotels);
     }
@@ -18,21 +20,33 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div className="flex justify-center">
       {hotels.map((hotel) => {
         return (
-          <div className="flex justify-center mt-[50px] gap-[30px]" key={hotel.id}>
-            <Image src={hotel.featuredImage} alt="hotel image" width={300} height={300} />
-            <div className="hotelDetails">
-              <h1>{hotel.name}</h1>
-              <h3>
-                <strong>Address:</strong> {hotel.address}
-              </h3>
-              <Link href={`/hotels/${hotel.id}`}>
-                <button className="btnDetails">View Details</button>
-              </Link>
+          <>
+            <div className="flex flex-col">
+              <div
+                className="flex justify-center mt-[50px] gap-[30px]"
+                key={hotel.id}
+              >
+                <Image
+                  src={hotel.featuredImage}
+                  alt="hotel image"
+                  width={300}
+                  height={300}
+                />
+                <div className="hotelDetails">
+                  <h1>{hotel.name}</h1>
+                  <h3>
+                    <strong>Address:</strong> {hotel.address}
+                  </h3>
+                  <Link href={`/hotels/${hotel.id}`}>
+                    <button className="btnDetails">View Details</button>
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
+          </>
         );
       })}
     </div>

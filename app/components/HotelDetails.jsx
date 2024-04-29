26f08d5a35a229310d1db8b8ed3223428b8834a0
kmumladze <ketevanmumladze17@gmail.com>
2024-04-29 19:56:38 +0400

    feat: new branche

--- app/components/HotelDetails.jsx
+++ app/components/HotelDetails.jsx
@@ -1,12 +1,20 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function HotelDetails({ hotel }) {
   console.log(hotel);
   return (
     <div>
-      <Image width={300} height={300} src={hotel.featuredImage} alt="hotel Image" />
+      {/* {hotel.map((room) => {
+        
+      })} */}
+      <Image
+        width={300}
+        height={300}
+        src={hotel.featuredImage}
+        alt="hotel Image"
+      />
       <h3>{hotel.name}</h3>
-      <p>{hotel.address}</p>
+      <p>Address:{hotel.address}</p>
       <button>See more</button>
     </div>
   );
